915a0aa60a38fe8105f50ac6240095c56ab75c4c
Joe Ng'ethe <>
2019-03-29 01:11:17 +0300

    install and configure prettier

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  tabWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  arrowParens: 'always',
+  printWidth: 100,
+  singleQuote: false,
+};
