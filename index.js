6fa79bcc015ef04b62055fdd52c9aa815edd93cd
Joe Ng'ethe <>
2019-03-28 13:05:55 +0300

    added a todo list

---
+++ index.js
@@ -0,0 +1,10 @@
+//todo list
+
+/*
+ 1. Build a graphql server using ApolloServer
+ 2. Analylize the shape of the response from our SWAPI REST API (https://swapi.co/)
+ 3. Define our schema
+ 4. Define our resolvers
+ 5. Pass our shema/typeDefs and resolvers to our ApolloServer instance
+ 6. Test out our API
+*/
