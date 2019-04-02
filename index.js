21d49c54eacd07cca0fca45a5e88d8432103ef1b
Joe Ng'ethe <>
2019-04-02 16:31:45 +0300

    added resolvers

--- index.js
+++ index.js
@@ -8,3 +8,70 @@
  5. Pass our shema/typeDefs and resolvers to our ApolloServer instance
  6. Test out our API
 */
+
+const { ApolloServer, gql } = require('apollo-server');
+const axios = require('axios');
+
+const typeDefs = gql`
+  type Query {
+    characters: [Person]
+    person(id: Int!): Person
+  }
+
+  type Person {
+    name: String
+    mass: String
+    films: [String]
+    species: [String]
+    vehicles: [String]
+    starships: [String]
+  }
+`;
+
+const BASEURL = `https://swapi.co/api`;
+
+const getPeople = () => {
+  return axios.get(`${BASEURL}/people`);
+};
+// const { name, mass, films, species, vehicles, starships } = results
+// return { name, mass, films, species, vehicles, starships };
+
+const resolvers = {
+  Query: {
+    characters: () => {
+      return axios.get(`${BASEURL}/people`).then(({ data }) => {
+        try {
+          return data.results.map(({ name, mass, films, species, vehicles, starships }) => {
+            return {
+              name,
+              mass,
+              films,
+              species,
+              vehicles,
+              starships
+            };
+          });
+        } catch (error) {
+          console.log(`something went wrong`, error);
+        }
+      });
+    },
+    person: (_, { id }) => {
+      return axios.get(`${BASEURL}/people/${id}`).then(({ data }) => {
+        const { name, mass, films, species, vehicles, starships } = data;
+        return {
+          name,
+          mass,
+          films,
+          species,
+          vehicles,
+          starships
+        };
+      });
+    }
+  }
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => console.log(`Sever running at ${url}`));
